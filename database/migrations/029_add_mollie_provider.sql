df3617bcc3b85b8eb3fe811c460f82df57418ec7
sanjarbaba <sanjarsadat@gmail.com>
2026-09-04 10:08:37 +0200

    Record Mollie provider database migration

---
+++ database/migrations/029_add_mollie_provider.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+ALTER TABLE purchase_events
+  DROP CONSTRAINT IF EXISTS purchase_events_provider_check;
+
+ALTER TABLE purchase_events
+  ADD CONSTRAINT purchase_events_provider_check
+  CHECK (provider IN ('mollie', 'stripe', 'apple', 'google', 'admin'));
+
+INSERT INTO schema_migrations (version, name)
+VALUES (29, 'add_mollie_provider')
+ON CONFLICT (version) DO NOTHING;
+
+COMMIT;
+
